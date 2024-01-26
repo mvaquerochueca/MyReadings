e9817c937d11ae03df6ffcd13eb1759144bdf0d0
mvaquerochueca <mario.vaquero.chueca@gmail.com>
2024-01-26 14:06:46 +0100

    Add covers and some styles. Starting the logic to update progress bar

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import COSMERE from './Cosmere.js';
+import BOOKS from './Books.js';
 
 const app = express();
 
@@ -11,23 +11,33 @@ app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
 
-app.get('/cosmere', (req, res) => {
-  res.json(COSMERE);
+app.get('/books', (req, res) => {
+  res.json(BOOKS);
 });
 
-app.get('/cosmere/:id', (req, res) => {
+app.get('/books/:id', (req, res) => {
   const id = req.params.id;
-  const cosmere = COSMERE.find((cosmere) => cosmere.id === +id);
-  res.json(cosmere);
+  const book = BOOKS.find((book) => book.id === +id);
+  res.json(book);
 });
 
-app.post('/cosmere', (req, res) => {
+app.post('/books', (req, res) => {
   const data = req.body;
   const newBook = {
-    id: COSMERE.length + 1,
+    id: BOOKS.length + 1,
     ...data,
   };
-  COSMERE.push(newBook);
+  BOOKS.push(newBook);
   res.status(201).send('Created');
   res.json(newBook);
+});
+
+app.patch('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  const book = BOOKS.find((book) => book.id === Number(id));
+  const index = BOOKS.indexOf(book);
+  const updatedBook = { ...book, ...data };
+  BOOKS[index] = updatedBook;
+  res.json(updatedBook);
 });
--- App/src/App.jsx
+++ App/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import './index.css';
 
-const apiUrl = 'http://localhost:3000/cosmere';
+const apiUrl = 'http://localhost:3000/books';
 function App() {
   const [book, setBook] = useState([]);
 
@@ -16,18 +16,50 @@ function App() {
     getData();
   }, []);
 
+  function handleUpdatePages() {
+    const apiUrlBook = 'http://localhost:3000/books/:id';
+    const inputPagesValue = document.getElementById('actualPages');
+
+    fetch(apiUrlBook, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        pagesRead: inputPagesValue.value,
+      }),
+    });
+  }
+
   return (
     <>
-      <h1 className='font-bold text-2xl justify-center flex'>Cosmere</h1>
-      <ul className='grid grid-cols-2 md:grid-cols-3 gap-4'>
-        {book.map((cosmere) => (
-          <li key={cosmere.id}>
-            Title - {cosmere.title}
+      <h1 className='font-bold text-2xl justify-center flex'>
+        My Reading List
+      </h1>
+      <ul className='grid grid-cols-2 md:grid-cols-3 ml-4 gap-2  mb-40'>
+        {book.map((book) => (
+          <li key={book.id} className=''>
+            {book.title}
             <br />
-            Author - {cosmere.author}
+            Author - {book.author}
             <br />
-            Cosmere Planet - {cosmere.planet}
-            <img src={cosmere.image} alt={cosmere.title} className='w-40 ' />
+            Book Planet - {book.planet}
+            <img src={book.image} alt={book.title} className='w-40 ' />
+            <progress
+              value={book.pagesRead}
+              max={book.totalPages}
+              className='[&::-webkit-progress-bar]:rounded-md [&::-webkit-progress-value]:rounded-lg   [&::-webkit-progress-bar]:bg-slate-300 [&::-webkit-progress-value]:bg-green-400 [&::-moz-progress-bar]:bg-black-900 mt-2 md:ml-2'></progress>
+            <input
+              type='text'
+              placeholder='Pages'
+              className='border border-slate-600 rounded-lg text-center md:ml-2'
+              id='actualPages'
+            />
+            <button
+              onClick={handleUpdatePages}
+              className='bg-blue-500 hover:bg-blue-700 text-white m-2  px-2 rounded'>
+              Update
+            </button>
           </li>
         ))}
       </ul>
